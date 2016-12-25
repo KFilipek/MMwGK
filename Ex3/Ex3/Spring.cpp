71b757facfa2d6b40e14062cb4934dd2faf81370
vityoube <vityoube@gmail.com>
2016-12-26 00:13:55 +0100

    Modificated spring for opengl (not so good yet)

--- Ex3/Ex3/Spring.cpp
+++ Ex3/Ex3/Spring.cpp
@@ -14,22 +14,35 @@ Spring::~Spring()
 void Spring::draw() {
 	glEnable(GL_DEPTH);
 	glDisable(GL_TEXTURE_2D);
-	glPushMatrix();
+	GLfloat u[1001], t[1001], spring_X[1001], spring_Y[1001], spring_Z[1001];
 	glTranslated(0.0, 0.0, -2.0);
-	GLfloat spring_X = 0.0f, spring_Y = 0.0f, spring_Z = 0.0f;
-	glBegin(GL_LINE_STRIP);
-	glColor4f(0.0f, 0.0f, 0.0f, 1.0f);
-	for (t = 0; t <= 8 * PI; t += T_STEP) {
-		for (u = 0; u <= 2 * PI; u += U_STEP) {
-			spring_X_step = (3 + (cos(u / 180)));
-			spring_Y_step = (3 + (sin(u / 180)));
-			spring_Z_step = (sin(u / 180));
-			spring_X = (cos(t / 180))*spring_X_step;
-			spring_Y = (sin(t / 180))*spring_Y_step;
-			spring_Z = (0.6*t) + spring_Z_step;
-			glVertex3f(spring_X, spring_Y, spring_Z);
+	glRotatef(360.0f, 1.0f, 0.0f, 0.0f);
+	for (int i = 0; i < 1001; i++) {
+		t[i] = 0.0f; u[i] = 0.0f;
+		spring_X[i] = 0.0f; spring_Y[i] = 0.0f; spring_Z[i] = 0.0f;
+	}
+	for (int j = 0; j < 10; j++) {
+		GLfloat spring_x_step = j*1.0f, spring_y_step = j*5.0f, spring_z_step = j*1.0f;
+		for (int i = 0; i < 100; i++) {
+			spring_X[i] = (cos(t[i]))*(3 + (cos(u[i])));
+			spring_Y[i] = (sin(t[i]))*(3 + (cos(u[i])));
+			spring_Z[i] = (0.6*t[i]) + (sin(u[i]));
+			
+			t[i+1] =t[i]+ STEP;
+			u[i+1] =u[i]+ STEP;
+			if (u[i] >= (2 * PI/180))
+				u[i] -= 2 * PI/180;
 		}
 	}
+	spring_X[1000] = (cos(t[1000]))*(3 + (cos(u[1000])));
+	spring_Y[1000] = (sin(t[1000]))*(3 + (cos(u[1000])));
+	spring_Z[1000] = (0.6*t[1000]) + (sin(u[1000]));
+	
+	glBegin(GL_LINE_STRIP);
+	glColor4f(0.0f, 0.0f, 0.0f, 1.0f);
+	for (int i = 0; i < 1001; i++) {
+		glVertex3f(spring_X[i], spring_Y[i]+10.0f, spring_Z[i]);
+	}	
 	glEnd();
-	glPopMatrix();
+	
 }
--- Ex3/Ex3/Spring.h
+++ Ex3/Ex3/Spring.h
@@ -6,11 +6,9 @@
 class Spring
 {
 protected:
-	const GLfloat U_STEP = (GLfloat)(2 * PI / 10), T_STEP = (GLfloat)(8 * PI / 10);
+	const GLfloat STEP = 8 * PI / 1800;
 	const GLfloat spring_size = 10.0f;
-	GLfloat spring_X_step = 0.0f, spring_Y_step = 5.0f, spring_Z_step = 0.0f;
 	GLfloat u, t;
-	int n = 200;
 public:
 	Spring();
 	~Spring();
